6926573ab915007c5d168579a73a5b1c3c2b41a4
quanphanhong <18520239@gm.uit.edu.vn>
2021-06-08 14:32:05 +0700

    Create APIs

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCbyZbPXpu13VLHn56HPi4cVP_nwkUge8E",
+    authDomain: "todoapp-c4e1c.firebaseapp.com",
+    projectId: "todoapp-c4e1c",
+    storageBucket: "todoapp-c4e1c.appspot.com",
+    messagingSenderId: "252571178767",
+    appId: "1:252571178767:web:b1f996fb87a6cf4309507a",
+    measurementId: "G-0K7GGB22NF"
+};
